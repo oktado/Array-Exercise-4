5abce31a27f029f53b02db4b06e056741b8af0d6
Oktado Putra <oktado.putra@gmail.com>
2020-10-23 09:02:09 +0700

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+// var DataPertama = ['Math','English','Programming']
+// let DataKedua   = ['Geography','Spanish','Programming']
+
+// DataBaru = DataKedua.map(function(e){
+//     return e == DataPertama[2];
+
+// });
+// console.log(DataBaru);
+
+
+// var DataPertama = ['Math','English','Programming']
+// var DataKedua   = ['Geography','Spanish','Programming']
+
+// for(var i = 0; i < DataPertama.length; i++) {
+//     console.log(DataPertama[i] == DataKedua[2]);
+// }
+
+
+var DataPertama = ['Math','English','Programming']
+var DataKedua   = ['Geography','Spanish','Programming']
+
+DataPertama.map(function(i){
+ console.log(i == DataKedua[2]);
+})
